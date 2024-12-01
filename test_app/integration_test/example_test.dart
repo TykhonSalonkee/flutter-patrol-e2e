483ae41735bc93148d096c7377f413d8098ec4bb
Tykhon <tykhon@salonkee.lu>
2024-12-01 22:09:40 +0100

    first integration test

---
+++ test_app/integration_test/example_test.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+void main() {
+  patrolTest(
+    'counter state is the same after going to home and switching apps',
+    ($) async {
+      await $.pumpWidgetAndSettle(
+        MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(title: const Text('app')),
+            backgroundColor: Colors.blue,
+          ),
+        ),
+      );
+
+      expect($('app'), findsOneWidget);
+      if (!Platform.isMacOS) {
+        await $.native.pressHome();
+      }
+    },
+  );
+}
