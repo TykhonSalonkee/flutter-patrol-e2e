2aa91b6ad1f26384009f89928a382ac983b95855
Tykhon <tykhon@salonkee.lu>
2024-12-05 17:38:15 +0100

    finalize ios setup

--- test_app/integration_test/example_test.dart
+++ test_app/integration_test/example_test.dart
@@ -8,6 +8,7 @@ void main() {
   patrolTest(
     'counter state is the same after going to home and switching apps',
     ($) async {
+      // Replace later with your app's main widget
       await $.pumpWidgetAndSettle(
         MaterialApp(
           home: Scaffold(
